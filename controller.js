149b280d2d52fa68d87a5d4c789aa08fad7f75df
Droffilc-codes <cliffordchisom@gmail.com>
2024-03-11 10:31:19 +0100

    Monday update

--- controller.js
+++ controller.js
@@ -53,32 +53,40 @@ playButton.addEventListener('click', clickPlayButton);
 
 function clickPlayButton() {
     // Bring back top image - randomly
-    location.reload();
-    
+    // location.reload();
+    controller1()
+
 }
 
 // The game controller
-controller1()
 
 
 function controller1 (){
-//re-order the arrays of Faces
-// theFaces.sort(()=> 0.5 - Math.random() )
+
 let randomValue = Math.floor(Math.random() * theFaces.length)
 console.log(randomValue)
-
 
 //Set the image to be matched
 projectedImage.src = theFaces[randomValue].img
 
 //Store the image to be matched with the id
 let loadedImage = []
+if(loadedImage.length>0){
+    loadedImage.length = 0
+}
 
 loadedImage.push(theFaces[randomValue].id)
 
+ // Test
+ theFirstBox.style.display = 'block'
+if(theFirstBox.style.display == 'none'){
+    console.log("It will not show")
+}else{
+    console.log("It will show")
 
+}
 //Show the face to be matched and then remove it
-let removeTopImage = setInterval(()=>{theFirstBox.style.display = 'none'}, 1500)
+let removeTopImage = setInterval(()=>{theFirstBox.style.display = 'none'}, 2500)
 
 //Reel out all the faces in the database
 let currentFaces = theFaces.length
@@ -88,25 +96,30 @@ function endRandomFaces(){
     console.log(currentFaces)
     userChoice.src = theFaces[currentFaces].img
     userChoice.setAttribute('id', theFaces[currentFaces].id)
+    console.log(loadedImage[0])
     if(currentFaces === 0){
         clearInterval(countDownFaces)
     }
 }
 
 let countDownFaces = setInterval(endRandomFaces, 2000)
-
 //Click and see the id number
 userChoice.addEventListener('click', (e)=>{
     let getId = e.target.id
     let getIdReal = parseInt(getId)
 
-   
+   // If nothing is selected -- tell me
     if(loadedImage[0] === getIdReal){
+        console.log(loadedImage[0], getIdReal)
         console.log("They Matched!")
         clearInterval(countDownFaces)
+        // clearInterval(removeTopImage)
     }else{
+        console.log(loadedImage[0], getIdReal)
         console.log("No Match!")
         clearInterval(countDownFaces)
+        // clearInterval(removeTopImage)
+
     }
 })
 
